f1dfcd145396686837573138ac3cb0032c3cdfee
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-12-11 10:31:14 -0300

    add comments

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ app.use( express.urlencoded( { extended: true } ) );
 app.use(morgan("tiny"));
 app.post( '/enviar_paquete', enviarPaqueteHandler );
 
-
+var counter = 0;
 function enviarPaqueteHandler ( req, res ) {
+    console.log( "-----------------------------------------" );
+    console.log( "Llego un paquete: " + req.query.paquete )
     const packet = lora_packet.fromWire(Buffer.from(req.query.paquete, "hex"));
 
     // debug: prints out contents
     // - contents depend on packet type
     // - contents are named based on LoRa spec
-    console.log("packet.toString()=\n" + packet);
+    console.log("Paquete decodificado\n" + packet);
 
     // e.g. retrieve payload elements
     console.log("packet MIC=" + packet.MIC.toString("hex"));
@@ -30,9 +32,10 @@ function enviarPaqueteHandler ( req, res ) {
     // decrypt payload
     const AppSKey = Buffer.from("ec925802ae430ca77fd3dd73cb2cc588", "hex");
     console.log("Decrypted (ASCII)='" + lora_packet.decrypt(packet, AppSKey, NwkSKey).toString() + "'");
-    console.log("Decrypted (hex)='0x" + lora_packet.decrypt(packet, AppSKey, NwkSKey).toString("hex") + "'");
-    console.log("-----------------------------------------")
-    // create a packet
+    console.log( "Decrypted (hex)='0x" + lora_packet.decrypt( packet, AppSKey, NwkSKey ).toString( "hex" ) + "'" );
+    
+    // Crear Paquete
+    console.log( "Creando paquete ACK" );
     const constructedPacket = lora_packet.fromFields(
         {
             MType: "Unconfirmed Data Down", // (default)
@@ -43,12 +46,14 @@ function enviarPaqueteHandler ( req, res ) {
                 ADRACKReq: false, // default = false
                 FPending: false, // default = false
             },
-            FCnt: Buffer.from("0003", "hex"), // can supply a buffer or a number
+            FCnt: counter, // can supply a buffer or a number
             payload: "test",
         },
         Buffer.from("ec925802ae430ca77fd3dd73cb2cc588", "hex"), // AppSKey
         Buffer.from("44024241ed4ce9a68c6a8bc055233fd3", "hex") // NwkSKey
     );
+    counter++;
+    console.log( "Paquete ACK creado" );
     const wireFormatPacket = constructedPacket.getPHYPayload();
     const ackMessage =  wireFormatPacket.toString("hex");
 
